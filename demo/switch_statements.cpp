31c6b0161c3af2917a02747d36eab9aef32a60a6
Om Mahajna <omrmahajan9477@gmail.com>
2024-10-10 11:39:58 +0530

    this is important now

---
+++ demo/switch_statements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std ;
+
+int main (){
+
+int num = 2 ;
+
+switch (num)
+{
+case 1 :
+    cout<< " This is 1st statement " << endl ;
+    break;
+
+case 2 :
+    cout<< " This is 2nd statement " << endl ;
+    break;
+
+default:
+    cout << " This is default condition " << endl ;
+    
+}
+
+
+    return 0 ;
+}
