3d54397dd82d5efbdfd1d070c68d7e3bd5592b60
Om Mahajna <omrmahajan9477@gmail.com>
2024-10-11 17:31:39 +0530

    even odd finder

--- demo/functions_basics.cpp
+++ demo/functions_basics.cpp
@@ -1,23 +1,26 @@
 #include <iostream>
 using namespace std ;
 
-int pwr (int a , int b)
-  {
- 
-     int ans = 1 ;
-
-     for (int i = 1 ; i <= b ; i++){
-         ans = ans * a ;
-     }
-    //  cout << ans ;
-    //    return ans ;
+bool isEven(int a){
+    if (a&1){
+        return 0 ;
+    }
+    else {
+        return 1 ;
+    }
 }
 
 int main (){
 
-int kl = pwr (10,2);
-cout<< kl << endl ;
+int num ;
+cin >> num ;
+if(isEven(num)) {
+        cout <<" Number is Even" <<endl;
+    }
+    else {
+        cout<< " Number is Odd" <<endl;
+    }
+
 
     return 0 ;
-
 }
